b1fef2124cda8f59224613c82f934e31632cd20c
mayragann <mayragann@gmail.com>
2022-05-09 16:16:11 -0400

    Added Human Class

--- RPSLS/RPSLS/Human.cs
+++ RPSLS/RPSLS/Human.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RPSLS
+namespace RockPaperScissorsLizardSpock
 {
-    internal class Human
+    class Human : Player
     {
+
     }
 }
--- RPSLS/RPSLS/Player.cs
+++ RPSLS/RPSLS/Player.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rock_Paper_Scissors_Lizard_Spock
+namespace RockPaperScissorsLizardSpock
 {
-    public abstract class Player
+    public class Player
     {
         public string name;
         public string choice;
