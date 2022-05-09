a25dee1cc74bc4096c3982ac3a7136ad4129a2c9
mayragann <mayragann@gmail.com>
2022-05-09 16:13:36 -0400

    Added Player Class

--- RPSLS/RPSLS/Player.cs
+++ RPSLS/RPSLS/Player.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace RPSLS_Csharp
+namespace Rock_Paper_Scissors_Lizard_Spock
 {
-    class Program
+    public abstract class Player
     {
-        static void Main(string[] args)
+        public string name;
+        public string choice;
+        public Random AirRandomOption = new Random();
+
+        public Player()
+        { 
+        
+        }
+        public void GetPlayerName()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("\nPlease Enter Player Name: ");
+            name = (Console.ReadLine());
+        }
+
+        public virtual void ChooseOption()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\n{name} Choose Your Option: \n Rock, Paper, Scissors, Lizard, or Spock");
+            choice = (Console.ReadLine());
+            switch (choice)
+            {
+                case "Rock":
+                    Console.WriteLine($"\n{name} Choose Rock");
+                    break;
+                case "Paper":
+                    Console.WriteLine($"\n{name} Choose Paper");
+                    break;
+                case "Scissors":
+                    Console.WriteLine($"\n{name} Choose Scissors");
+                    break;
+                case "Lizard":
+                    Console.WriteLine($"\n{name} Choose Lizardk");
+                    break;
+                case "Spock":
+                    Console.WriteLine($"\n{name} Choose Spock");
+                    break;
+                default:
+                    Console.WriteLine($"\n{name}, PICK THE CORRECT OPTION");
+                    break;
+            }
         }
     }
 }
--- RPSLS/RPSLS/Program.cs
+++ RPSLS/RPSLS/Program.cs
@@ -1,25 +1,21 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rock_Paper_Scissors_Lizard_Spock
+namespace RockPaperScissorsLizardSpock
 {
-    class Program {
+    class Program
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would You like To Play Rock Paper Scissors Lizard Spock? Type y for yes and n for no: ");
-            string answer = Console.ReadLine();
-            switch (answer.ToLower())
-            {
-                case "y":
-                    Game NewGame = new Game();
-                    NewGame.RunGame();
-                    break;
-                case "n";
-                    break;
-            }
-            Console.Read();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Game gameStart = new Game();
+            gameStart.StartGame();
+            Console.ReadLine();
         }
-    } }
+    }
+}
